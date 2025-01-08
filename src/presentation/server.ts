fbb83ef4752896559f8a7217102b63031afc5c93
DiegoFSV12 <dsanchezver@ucvvirtual.edu.pe>
2025-01-08 14:28:41 -0500

    CheckService creado

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,3 +1,4 @@
+import { CheckService } from '../domain/use-cases/checks/check-service';
 import {CronService} from './cron/cron-service';
 
 export class Server{
@@ -8,6 +9,7 @@ export class Server{
             ()=>{
                 const date = new Date();
                 console.log('Mensaje cada 5 segundos',date);
+                new CheckService().execute('https://www.google.com');
             }
         );
         
