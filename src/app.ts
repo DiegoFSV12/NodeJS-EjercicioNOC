b7a5d98dbd5680747bafb046fac6ea23eba782f7
DiegoFSV12 <dsanchezver@ucvvirtual.edu.pe>
2025-01-16 14:58:43 -0500

    inserciones y lectura de datos en prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import { PrismaClient } from '@prisma/client';
 import {envs} from './config/plugins/envs.plugin';
 import { LogModel } from './data/mongo-conf';
 import { MongoDatabase } from './data/mongo-conf/init';
@@ -12,6 +13,28 @@ async function main(){//Espera a que se connecte al servidor
     mongoURL: envs.MONGO_URL,
     dbName: envs.MONGO_DB_NAME
   });
+//Inserciones en PRISMA
+// const prisma = new PrismaClient();
+// const newLog = await prisma.logModel.create({
+//   data:{
+//     level: 'HIGH',
+//     message: 'Test message',
+//     origin: 'App.ts'
+//   }
+// });
+// console.log({newLog});
+
+//Lectura en PRISMA
+const prisma = new PrismaClient();
+const logs = await prisma.logModel.findMany({
+  where:{
+    level: 'MEDIUM'
+  }
+});
+console.log(logs);
+
+
+
   // //Crear un collection y documento (tabla y registro)
   // const newLog = await LogModel.create({
   //   message: 'Test message desde mongo',
@@ -20,7 +43,7 @@ async function main(){//Espera a que se connecte al servidor
   // });
   // await newLog.save();
   // console.log(newLog)
-  Server.start();
+  //Server.start();
 
   // const logs = await LogModel.find();
   // console.log(logs[2].level);
