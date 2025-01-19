6f32dcac62d51c6d5fc2cd62e86da6ad804e4de6
DiegoFSV12 <dsanchezver@ucvvirtual.edu.pe>
2025-01-19 14:56:41 -0500

    CheckService testing

---
+++ src/domain/use-cases/checks/check-service.test.ts
@@ -0,0 +1,34 @@
+import { LogEntity } from "../../entities/log.entity";
+import { CheckService } from "./check-service";
+
+describe('check-service.ts',()=>{
+    const mockRepository = {
+        saveLog: jest.fn(),
+        getLogs: jest.fn()
+    }
+    const successCallback = jest.fn();
+    const errorCallback = jest.fn();
+    const checkService = new CheckService(mockRepository,successCallback,errorCallback);
+
+    beforeEach(()=>{
+        jest.clearAllMocks();
+    });
+
+    test('should call successCallback when fetch returns true',async()=>{
+        
+        const wasOk = await checkService.execute('https://google.com');
+        expect(wasOk).toBe(true);
+        expect(successCallback).toHaveBeenCalled();
+        expect(errorCallback).not.toHaveBeenCalled();
+        expect(mockRepository.saveLog).toBeCalledWith(expect.any(LogEntity));
+    });
+
+    test('should call successCallback when fetch returns false',async()=>{
+        
+        const wasOk = await checkService.execute('httpTESTs://google.com');
+        expect(wasOk).toBe(false);
+        expect(successCallback).not.toHaveBeenCalled();
+        expect(errorCallback).toHaveBeenCalled();
+        expect(mockRepository.saveLog).toBeCalledWith(expect.any(LogEntity));
+    });
+});
